7bfaeaf7303c4df870e8a5938be292cbae53606b
fatimazahraelbakkali78-blip <fatimazahraelbakkali78@gmail.com>
2026-03-31 17:11:50 +0100

    Rapport

---
+++ api/Rapport.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Rapport de stage</h1>
+    <p>Durant mon stage de développement digital, j'ai eu l'opportunité
+</body>
+</html>
