048cece3bbed978f29e0a22ea9a43c3554cdcff8
fatimazahraelbakkali78-blip <fatimazahraelbakkali78@gmail.com>
2026-04-07 13:13:20 +0100

    jj

--- api/tp 1.php
+++ api/tp 1.php
@@ -6,12 +6,30 @@
     <title>Document</title>
 </head>
 <body>
-    
-<?php
-// api/resultat.php
-$nom = $_POST["nom"];
-$prenom = $_POST["prenom"];
-echo "Bienvenue " . $nom . " " . $prenom;
-?>
+
+<!-- index.php --> 
+<?php 
+for ($i = 5; $i <= 100; $i += 5) { 
+ echo "<a href='resultat.php?val=$i'>$i</a> "; 
+} 
+?> 
+<!-- resultat.php --> 
+<?php 
+$val = $_GET["val"]; 
+echo "Vous avez choisi : " . $val; 
+?> 
+<br>
+<!-- formulaire.html --> 
+<form method="POST" action="resultat.php"> 
+ Nom : <input type="text" name="nom"><br> 
+ Prénom : <input type="text" name="prenom"><br> 
+ <input type="submit" value="Valider"> 
+</form> 
+<!-- resultat.php --> 
+<?php 
+$nom = $_POST["nom"]; 
+$prenom = $_POST["prenom"]; 
+echo "Bienvenue " . $nom . " " . $prenom; 
+?> 
 </body>
 </html>
